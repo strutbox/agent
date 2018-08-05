4242ba9317b56c8229213d8769808859477a1910
Matt Robenolt <matt@ydekproductions.com>
2018-08-05 14:42:59 -0700

    Add a few more debugging commands

--- http.go
+++ http.go
@@ -2,15 +2,58 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"os/exec"
 	"time"
 )
 
 func runHttpService() {
 	log.Println("http: starting")
 	http.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("OK\n"))
+	})
+	http.HandleFunc("/version", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "serial=%s version=%s build=%s\n", SerialNumber(), Version, BuildVersion)
+	})
+	http.HandleFunc("/status", func(w http.ResponseWriter, r *http.Request) {
+		cmd := exec.Command("systemctl", "status", "strut.service")
+		cmd.Stdout = w
+		cmd.Stderr = w
+		if err := cmd.Run(); err != nil {
+			fmt.Fprint(w, err.Error())
+		}
+		w.Write([]byte{'\n'})
+	})
+	http.HandleFunc("/logs", func(w http.ResponseWriter, r *http.Request) {
+		cmd := exec.Command("journalctl", "-u", "strut.service")
+		cmd.Stdout = w
+		cmd.Stderr = w
+		if err := cmd.Run(); err != nil {
+			fmt.Fprint(w, err.Error())
+		}
+		w.Write([]byte{'\n'})
+	})
+	http.HandleFunc("/cache", func(w http.ResponseWriter, r *http.Request) {
+		cmd := exec.Command("ls", "-lh", cacheManager.dir)
+		cmd.Stdout = w
+		cmd.Stderr = w
+		if err := cmd.Run(); err != nil {
+			fmt.Fprint(w, err.Error())
+		}
+		w.Write([]byte{'\n'})
+	})
+	http.HandleFunc("/exec", func(w http.ResponseWriter, r *http.Request) {
+		body, _ := ioutil.ReadAll(r.Body)
+		r.Body.Close()
+		cmd := exec.Command("bash", "-c", string(body))
+		cmd.Stdout = w
+		cmd.Stderr = w
+		if err := cmd.Run(); err != nil {
+			fmt.Fprint(w, err.Error())
+		}
+		w.Write([]byte{'\n'})
 	})
 
 	for {
